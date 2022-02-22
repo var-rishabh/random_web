dcae11f4b31712323536104fb082c95d3da30c62
Rishabh Varshney <right.rishabh@gmail.com>
2022-02-23 01:20:35 +0530

    edit 2

--- src/basicJS.js
+++ src/basicJS.js
@@ -15,36 +15,44 @@ const Movies = [
 
 const Numbers = [11,22,33,44,55,66,77,88,99]
 
+const User = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 21,
+    birthYear: 2001,
+    bio: "Stiident"
+}
+
 // ==================================================================================================================================================================
 
 
 // types of loop
 
-for (var i = 0; i < Languages.length; i++) {
-    // console.log(Languages[i]);
-}
+// for (var i = 0; i < Languages.length; i++) {
+//     console.log(Languages[i]);
+// }
 
-for (var lang of Languages) {                                  // "of" for arrays or characters and "in" for objects
-    // console.log(lang);
-}
+// for (var lang of Languages) {                                  // "of" for arrays or characters and "in" for objects
+//     console.log(lang);
+// }
 
-function print(element) {
-    // console.log(element);
-}
+// function print(element) {
+//     console.log(element);
+// }
 
-Languages.forEach(lang => {
-    // console.log(lang);
-});
+// Languages.forEach(lang => {
+//     console.log(lang);
+// });
 
-Languages.forEach(print);                                           // alternative of code line 16 - 18 
+// Languages.forEach(print);                                           // alternative of code line 16 - 18 
 
-var x = Numbers.map(function(num) {                                      // retuns an array of mapped elements
-    return num*4;
-})
+// var x = Numbers.map(function(num) {                                      // retuns an array of mapped elements
+//     return num*4;
+// })
 
-Movies.forEach(function(movie) {                                    // forEach loop to iterate over Object
-    // console.log(`${movie.Title} (${movie.Year})`);
-})
+// Movies.forEach(function(movie) {                                    // forEach loop to iterate over Object
+//     console.log(`${movie.Title} (${movie.Year})`);
+// })
 
 
 // ==================================================================================================================================================================
@@ -59,11 +67,65 @@ const myMath = {
     cube(num) {                                                    // loca.cube(value)
         return num*num*num;
     },
-    print() {
-        console.log(`Final Value : ${this.value}`);                // "this" keyword refer to the object at the left of the ".", not to the window object    
-        // console.log(this);       
-    }
+    type:function () {
+        return `Final Value : ${this.value}`;                // "this" keyword refer to the object at the left of the ".", not to the window object
+    },                                                       
+    shout: function () {
+        setTimeout(() => {                                   // but "this" keyword refers to window object when used in arrow function
+            console.log(this.type())
+        }, 3000);
+    }    
 };
 
 
 // ==================================================================================================================================================================
+
+
+
+// ES6 -- Map, Filter, Arrow Function, Default Parameters and ForEach
+
+// setTimeout(() => {                                                  // for single time
+//     console.log("Hello world!");
+// }, 2000);  
+
+// let num = 0;
+// const id = setInterval(() => {                                      // for infinite interval
+//     if (num == 6) {
+//         clearInterval(id);
+//     };
+//     console.log(num);
+//     num += 1;
+// }, 500)
+
+
+const x = Movies.filter(m => m.Rating < 7 )                           // filter selected object items
+const y = Movies.filter(m => m.Rating > 7 ).map(m => m.Title.toUpperCase())
+
+Numbers.every(num => num < 50);                                       // condition on every element must be true
+Numbers.some(num => num < 50);                                        // condition on one element must be true
+
+const solo = Movies.reduce((min, num) => num.Rating < min.Rating ? num : min);          // return only single element
+
+
+// Spread (...)
+Math.max(...Numbers)
+// console.log(..."rishabh")           // these both are same
+// console.log("r", "i", "s", "h", "a", 'b', "h") 
+
+const all_data = [1, 2, 3, ...Numbers, ...Languages, "Super Duper"];        // same can be done with objects
+var new_array = [..."radioacitve"];
+
+
+// REST parameters
+function sum1(...nums) {
+    return nums.reduce((total, ele) => total + ele);
+}
+var sum2 = (...nums) => (nums.reduce((total, ele) => total + ele))
+
+
+// destructing
+var [uae, westBengal, ...other] = Languages;                    // Array
+
+var { firstName, lastName : surName, death = "N/A"} = User;     // Object
+
+Movies.filter(({ Gross }) => Gross > 300);
